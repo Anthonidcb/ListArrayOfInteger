dbcc63451e4b8ffff0c76923ef8c0d612a34f061
Ânthoni da Costa <anthonibonini@gmail.com>
2025-08-26 14:13:17 +0000

    ATT_AULA

--- src/com/anthoni/main/ListArrayOfInteger.java
+++ src/com/anthoni/main/ListArrayOfInteger.java
@@ -74,5 +74,36 @@ public class ListArrayOfInteger {
         }
     }
 
-    
+    public boolean remove(Integer element){
+        for(int i = 0; i < count; i++){
+            if(element.equals(data[i])){
+                for(int j = i; j < count - 1; j++){
+                    data[j] = data[j + 1];
+                }
+                data[count - 1] = null;
+                count--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer set(int index, Integer element){
+        if(index < 0 || index > = count){
+            throw new IndexOutOfBoundsException("ERRO" + index);
+            Integer aux = data[index];
+            data[index] = element;
+            return aux;
+        }
+    }
+
+    public boolean contains(Integer element){
+        for (int i = 0; i<count; i++){
+            if(element.equals(data[i])){
+                return true;
+            }
+            return false;
+        }
+    }
+
 } 
